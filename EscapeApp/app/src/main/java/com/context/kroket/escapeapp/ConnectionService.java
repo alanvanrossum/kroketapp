fa874e336588e4032d48c5f47a85ec80086e22d3
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-05-17 13:19:43 +0200

    Go back to waiting screen when minigame done message is received

--- EscapeApp/app/src/main/java/com/context/kroket/escapeapp/ConnectionService.java
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/ConnectionService.java
@@ -86,6 +86,8 @@ public class ConnectionService extends Service {
                     startA();
                 } else if (action.contentEquals("startB")) {
                     startB();
+                } else if (action.contentEquals("minigameDone")) {
+                    endMinigame();
                 }
             }
 
@@ -100,6 +102,12 @@ public class ConnectionService extends Service {
 
     public void startB() {
         Intent dialogIntent = new Intent(this, Game_B_Activity.class);
+        dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(dialogIntent);
+    }
+
+    public void endMinigame() {
+        Intent dialogIntent = new Intent(this, WaitingActivity.class);
         dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(dialogIntent);
     }
