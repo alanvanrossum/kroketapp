edda9b16cb3fbf1c460c578adf8822e40231302b
harveyvanveltom <harvey-shadow@hotmail.com>
2016-05-19 20:13:12 +0200

    attempt #2 to solve travis ci error.

--- EscapeApp/app/src/main/java/com/context/kroket/escapeapp/MainActivity.java
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/MainActivity.java
@@ -117,12 +117,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(wakeupScreen == true) {
+        
             KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
             KeyguardManager.KeyguardLock keyguardLock = km.newKeyguardLock("TAG");
             keyguardLock.disableKeyguard();
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        }
+
         setContentView(R.layout.activity_main);
     }
 
