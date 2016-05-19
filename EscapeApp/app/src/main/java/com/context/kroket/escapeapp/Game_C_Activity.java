efd963a8f1099216d0d3b0ae3c60bebb714c58b7
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-05-19 16:26:00 +0200

    Beginning new minigame (rectangle)

---
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/Game_C_Activity.java
@@ -0,0 +1,28 @@
+package com.context.kroket.escapeapp;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Game_C_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game__c_);
+    }
+
+    public void startC() {
+//        Drawable d = getResources().getDrawable(R.drawable.rectangle);
+//        d.setc
+//
+//         rec = ( findViewById(R.drawable.rectangle));
+//
+//        GradientDrawable bgShape = (GradientDrawable)btn.getBackground();
+//        bgShape.setColor(Color.BLACK);
+    }
+
+}
