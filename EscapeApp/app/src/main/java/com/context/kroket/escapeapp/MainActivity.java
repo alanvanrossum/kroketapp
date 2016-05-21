81c3a53e83a29da876b30d95738fe28e0158a053
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-05-21 12:20:03 +0200

    Remove unused imports

--- EscapeApp/app/src/main/java/com/context/kroket/escapeapp/MainActivity.java
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/MainActivity.java
@@ -1,12 +1,9 @@
 package com.context.kroket.escapeapp;
 
-import android.app.KeyguardManager;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
