e22b712582204f75603ae3024d691017510c8b8c
Alan van Rossum <alanvanrossum@gmail.com>
2016-06-02 13:09:11 +0200

    now logs the sequence

--- EscapeApp/app/src/main/java/com/context/kroket/escapeapp/minigames/F_Lock.java
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/minigames/F_Lock.java
@@ -9,17 +9,20 @@ import android.widget.TextView;
 
 import com.context.kroket.escapeapp.R;
 
+import java.util.ArrayList;
+
 public class F_Lock extends AppCompatActivity {
     ImageButton left,right;
     ImageView turnlock;
     boolean rotatingRight;
+    ArrayList<Integer> enteredSequence;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.f__lock);
         addListenersToArrows();
         turnlock = (ImageView) findViewById(R.id.turnlock);
-
+        enteredSequence = new ArrayList<Integer>();
     }
     private void addListenersToArrows() {
         left =(ImageButton) findViewById(R.id.arrowleft);
@@ -43,6 +46,8 @@ public class F_Lock extends AppCompatActivity {
     private void rotateRight() {
         if(!rotatingRight){
             rotatingRight=true;
+            addToSequence();
+            System.out.println(enteredSequence.toString());
         }
         rotate(9);
     }
@@ -51,12 +56,22 @@ public class F_Lock extends AppCompatActivity {
     private void rotateLeft() {
         if(rotatingRight){
             rotatingRight=false;
+            addToSequence();
+            System.out.println(enteredSequence.toString());
+
         }
         rotate(-9);
     }
 
+    private void addToSequence() {
+        if(turnlock.getRotation()>0)
+            enteredSequence.add(Math.round(40-(turnlock.getRotation()/9)));
+        else
+            enteredSequence.add(Math.round(turnlock.getRotation()/-9));
+    }
+
     private void rotate(int alpha) {
-        turnlock.setRotation(turnlock.getRotation()+alpha);
+        turnlock.setRotation((turnlock.getRotation()+alpha)%360);
     }
 
 }
