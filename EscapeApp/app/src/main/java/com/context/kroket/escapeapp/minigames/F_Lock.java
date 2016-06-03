8e16ef2767ceb1659b0370daa12c7961f3ebfa72
Alan van Rossum <alanvanrossum@gmail.com>
2016-06-03 12:36:50 +0200

    Added insane amounts of well-written JavaDoc.

--- EscapeApp/app/src/main/java/com/context/kroket/escapeapp/minigames/F_Lock.java
+++ EscapeApp/app/src/main/java/com/context/kroket/escapeapp/minigames/F_Lock.java
@@ -16,6 +16,15 @@ public class F_Lock extends AppCompatActivity {
     ImageView turnlock;
     boolean rotatingRight;
     ArrayList<Integer> enteredSequence;
+
+    /**
+     * This method adds listeners to the arrow pictures by calling addListenersToArrows()
+     * This method also sets the global variable imageView to match the turning lock, so it can be easily rotated.
+     * Finally, this method creates a simple arraylist of integers to store the current value of the lock everytime the direction is changed.
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,11 @@ public class F_Lock extends AppCompatActivity {
         turnlock = (ImageView) findViewById(R.id.turnlock);
         enteredSequence = new ArrayList<Integer>();
     }
+
+    /**
+     * This is an initializing method that is called once during the creation of the activity.
+     * This method adds a button function to the two arrows in the bottom of the screen, which make calls to rotateRight() and rotateLeft()
+     */
     private void addListenersToArrows() {
         left =(ImageButton) findViewById(R.id.arrowleft);
         left.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +57,10 @@ public class F_Lock extends AppCompatActivity {
         });
     }
 
+    /**
+     * This method is called when the RightArrow is pressed
+     * This method adds 9 to the rotation, and if the direction is changed it calls to addToSequence()
+     */
     private void rotateRight() {
         if(!rotatingRight){
             rotatingRight=true;
@@ -52,7 +70,10 @@ public class F_Lock extends AppCompatActivity {
         rotate(9);
     }
 
-
+    /**
+     * This method is called when the LeftArrow is pressed.
+     * This method subtracts 9 from the rotation, and if the direction is changed it calls to addToSequence()
+     */
     private void rotateLeft() {
         if(rotatingRight){
             rotatingRight=false;
@@ -63,6 +84,10 @@ public class F_Lock extends AppCompatActivity {
         rotate(-9);
     }
 
+    /**
+     * This method is called when the user switches from turning Left to turning Right or vice versa.
+     * This method adds the number the lock is currently at to the sequence.
+     */
     private void addToSequence() {
         if(turnlock.getRotation()>0)
             enteredSequence.add(Math.round(40-(turnlock.getRotation()/9)));
@@ -70,6 +95,10 @@ public class F_Lock extends AppCompatActivity {
             enteredSequence.add(Math.round(turnlock.getRotation()/-9));
     }
 
+    /**
+     * This method sets the rotation for the turning part of the lock. A rotation of 9 degrees corresponds to a rotation of one number.
+     * @param alpha The angle in which the lock must rotate. Can be 9 or -9 for positive or negative turning respectively
+     */
     private void rotate(int alpha) {
         turnlock.setRotation((turnlock.getRotation()+alpha)%360);
     }
